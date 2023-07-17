2f471a4581295b28675c7770445b5769bf101482
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-17 11:19:19 +0100

    29 - Injection Custom Applications Properties

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/pedro/springboot/demo/mydemoapp/rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/pedro/springboot/demo/mydemoapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.pedro.springboot.demo.mydemoapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
