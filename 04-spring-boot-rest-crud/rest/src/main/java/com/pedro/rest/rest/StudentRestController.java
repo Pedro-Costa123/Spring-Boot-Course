07386b92ae424172fa59a3eca76f0e0fb7569151
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-19 13:09:42 +0100

    103 - REST Path Variables

--- 04-spring-boot-rest-crud/rest/src/main/java/com/pedro/rest/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/rest/src/main/java/com/pedro/rest/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.pedro.rest.rest;
 
 import com.pedro.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             students.add(new Student("FIRSTNAME - " + i, "LASTNAME - " + i));
         }
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 
 }
