12118724019aaa2f2ce5803f7c9342e4071f732b
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-20 13:43:52 +0100

    150 - BCrypt Encryption

--- 05-spring-boot-rest-security/spring-boot-rest-security/src/main/java/com/pedro/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/spring-boot-rest-security/src/main/java/com/pedro/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -6,14 +6,40 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configure ->
+                configure
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasAnyRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAnyRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasAnyRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasAnyRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasAnyRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails maria = User.builder().username("maria").password("{noop}test123").roles("EMPLOYEE").build();
@@ -42,5 +68,6 @@ public class DemoSecurityConfig {
         return http.build();
 
     }
+    */
 
 }
