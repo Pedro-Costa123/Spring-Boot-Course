c66c024d1c642e97e46915db3b31c32ff7829932
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-20 10:41:16 +0100

    137 - Spring Data using REST Configs and Sorting

--- 04-spring-boot-rest-crud/crudddemo/src/main/java/com/pedro/springboot/crudddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/crudddemo/src/main/java/com/pedro/springboot/crudddemo/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.pedro.springboot.crudddemo.dao;
 import com.pedro.springboot.crudddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members") -> if we want to change from employees to members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
