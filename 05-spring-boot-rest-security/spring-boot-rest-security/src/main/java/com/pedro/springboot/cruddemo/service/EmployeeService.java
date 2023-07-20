4c7fd4ff70b3da284a4b55a5e553f2caaf65eb6a
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-20 10:53:12 +0100

    Section 5 - Base

---
+++ 05-spring-boot-rest-security/spring-boot-rest-security/src/main/java/com/pedro/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.pedro.springboot.cruddemo.service;
+
+import com.pedro.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
---
+++ 05-spring-boot-rest-security/spring-boot-rest-security/src/test/java/com/pedro/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
