8a40c9124e845ae9a7f20d85baa128d347aebc94
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-21 12:20:13 +0100

    174 - Delete Employee

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/pedro/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/pedro/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -57,6 +57,13 @@ public class EmployeeController {
         return "redirect:/employees/list";
     }
 
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+        employeeService.deleteById(id);
+
+        return "redirect:/employees/list";
+    }
+
 }
 
 
