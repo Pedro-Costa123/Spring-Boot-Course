ef024f5755b91bdec7b486f6a51451bee86313a5
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-24 12:36:59 +0100

    199 - Restrict URLs Based On Roles

--- 08-spring-boot-spring-mvc-security/demosecurity/src/main/java/com/pedro/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/demosecurity/src/main/java/com/pedro/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -34,7 +34,11 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(config -> config.anyRequest().authenticated())
+        http.authorizeHttpRequests(config -> config
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser").permitAll())
                         .logout(LogoutConfigurer::permitAll);
