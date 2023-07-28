eadb8c88ec25a705f91713f18d1474f90c96a26a
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-28 09:25:38 +0100

    304 - Pointcut Declarations

--- 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.pedro.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,10 +15,22 @@ public class MyDemoLoggingAspect {
 //    @Before("execution(* add*(com.pedro.aopdemo.Account))") //match param type
 //    @Before("execution(* add*(com.pedro.aopdemo.Account, ..))") //match with Account and any number of arguments
 //    @Before("execution(* add*(..))") //match on any parameters
-    @Before("execution(* com.pedro.aopdemo.dao.*.*(..))") //march any method on a package
+//    @Before("execution(* com.pedro.aopdemo.dao.*.*(..))") //march any method on a package
+
+
+    @Pointcut("execution(* com.pedro.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("Executing @Before advice on addAccount()");
 
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("Performing API Analytics");
     }
 
 
