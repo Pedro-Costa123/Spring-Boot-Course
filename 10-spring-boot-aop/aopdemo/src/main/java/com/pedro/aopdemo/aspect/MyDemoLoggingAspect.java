2306170748d5584bafac3851914b9d0040f159d6
Pedro Costa <pedro.da-costa@capgemini.com>
2023-07-28 11:26:48 +0100

    321 - @AfterReturningAdvice

--- 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,34 @@ package com.pedro.aopdemo.aspect;
 
 import com.pedro.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    @AfterReturning(pointcut = "execution(* com.pedro.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
+    public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("Executing @AfterReturning on method: " + method);
+        System.out.println("Result is: " + result);
+        convertAccountNamesToUpperCase(result);
+        System.out.println("Result is: " + result);
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+        for (Account account : result) {
+            account.setName(account.getName().toUpperCase());
+        }
+    }
 
     @Before("com.pedro.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint) {
--- 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/dao/AccountDAOImpl.java
+++ 10-spring-boot-aop/aopdemo/src/main/java/com/pedro/aopdemo/dao/AccountDAOImpl.java
@@ -3,11 +3,26 @@ package com.pedro.aopdemo.dao;
 import com.pedro.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
     private String name;
     private String serviceCode;
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> accounts = new ArrayList<>();
+        Account account = new Account("Name", "Level");
+        Account account1 = new Account("Name - 1", "Level - 1");
+        Account account2 = new Account("Name - 2", "Level - 2");
+        accounts.add(account);
+        accounts.add(account1);
+        accounts.add(account2);
+        return accounts;
+    }
 
     @Override
     public void addAccount(Account account, boolean vipFlap) {
